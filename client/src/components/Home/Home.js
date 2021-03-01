bf853128d7fbdf3654564cc6279a6dd959176ccc
sohamdutta <xzoham@gmail.com>
2021-03-01 11:53:19 -0500

    refactor of axios call

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,7 @@ import { ExitToAppSharp, SettingsApplications } from '@material-ui/icons'
 export default function Home() {
 
   useEffect(() => {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false')
       .then(res => {
         setCoinData(res.data)
       }).catch(error => console.log(error))
@@ -18,8 +18,8 @@ export default function Home() {
         <form>
           <input type="text" placeholder="Search" className="coin-input" />
         </form>
-
       </div>
+
     </div>
   )
 }
