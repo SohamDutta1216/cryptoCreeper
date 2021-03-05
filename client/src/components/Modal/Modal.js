bbecbd4cf011fe9a95a8562a309ffde2c915cea1
sohamdutta <xzoham@gmail.com>
2021-03-05 13:01:10 -0500

    working on login oauth

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -2,8 +2,14 @@ import React, { useState } from 'react'
 import { Button, Modal, Form } from 'semantic-ui-react'
 import { GoogleLogin } from 'react-google-login'
 
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+
 export default function AuthModal() {
+  const [form, setForm] = useState(initialState);
   const [open, setOpen] = useState(false)
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
 
 
   return (
@@ -14,44 +20,69 @@ export default function AuthModal() {
       open={open}
       trigger={<button class="ui orange button">Sign in</button>}
     >
-      <Modal.Header style={{ textAlign: 'center' }}>Sign in</Modal.Header>
-      <Modal.Content>
-        <Form>
-          <Form.Field>
+      <form class="ui form" style={{ margin: '5px' }}>
+        <div class='ui inverted segment'>
+          <Modal.Header style={{ textAlign: 'center', fontSize: '40px' }}>Sign In</Modal.Header>
+          <Modal.Content>
+
+            <div >
+
+              <label>First Name</label>
+              <input handleChange={handleChange} name="firstName" label="firstName"></input>
+            </div>
+
+            <div>
+              <label>Last Name</label>
+              <input handleChange={handleChange} name="lastName" label="lastName"></input>
+
+            </div>
+
             <label>Email</label>
-            <input placeholder='Email' type="email" />
-          </Form.Field>
-          <Form.Field>
+            <input
+              handleChange={handleChange}
+              placeholder='Email' type="email" />
+
             <label>Password</label>
-            <input placeholder='Password' type="password" />
-          </Form.Field>
+            <input
+              handleChange={handleChange}
+              placeholder='Password' type="password" />
 
-          <GoogleLogin
-            clientId="489312559331-8lcppthhivcgf1nkjt4kko6i90apfq33.apps.googleusercontent.com"
-            render={(renderProps) => (
-              <Button
-                onClick={renderProps.onClick} disabled={renderProps.disabled}
-                color="blue">
-                <i class="google icon"></i>  Continue with Google
-              </Button>
-            )}
-            cookiePolicy="single_host_origin"
-          />
 
-        </Form>
+            <div class="ui right aligned inverted segment" >
+              <button class='ui button' onClick={() => setOpen(false)}>
+                Close
+        </button>
+              <GoogleLogin
+                clientId="489312559331-8lcppthhivcgf1nkjt4kko6i90apfq33.apps.googleusercontent.com"
+                render={(renderProps) => (
+                  <button
+                    class='ui blue button'
+                    onClick={renderProps.onClick} disabled={renderProps.disabled}
+                  >
+                    <i class="google icon"></i>  Continue with Google
+                  </button>
+                )}
 
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='black' onClick={() => setOpen(false)}>
-          Close
-        </Button>
-        <Button
-          color="orange"
-          onClick={() => setOpen(false)}
-        >Sign In</Button>
+                cookiePolicy="single_host_origin"
+              />
+              <button
+                class='ui orange button'
+                type="submit"
+              >Sign In</button>
+            </div>
+          </Modal.Content>
+          <Modal.Actions>
 
-      </Modal.Actions>
-    </Modal>
+
+          </Modal.Actions>
+
+        </div>
+      </form>
+    </Modal >
+
+
+
+
   )
 }
 
