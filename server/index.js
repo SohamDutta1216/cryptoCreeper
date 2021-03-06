f325f41e0d3cf688b2c9a48992add3192deeb190
sohamdutta <xzoham@gmail.com>
2021-03-06 11:27:48 -0500

    user signup, signin, and oauth working on front and back end

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.js'
+
 
 const app = express();
 dotenv.config()
@@ -11,6 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000
 const CONNECTION_URL = process.env.CONNECTION_URL
